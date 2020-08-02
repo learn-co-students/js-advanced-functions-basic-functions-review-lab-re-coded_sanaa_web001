609f883525ee54fba15a31bcdead93fd22432aa1
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-08-02 23:18:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(act="roller-skate") {
+  return `This Saturday, I want to ${act}!`;
+  }
+
+function mondayWork(act="go to the office") {
+    return `This Monday, I will ${act}.`;
+}
+
+ function wrapAdjective(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`;
+    }
+  }
+  
+  var Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b;
+    },
+    multiply: function(a,b) {
+      return a * b;
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+  }
+  
+ function actionApplyer(start, ray) {
+    let a = start;
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a);
+    }
+  
+    return a;
+  }
