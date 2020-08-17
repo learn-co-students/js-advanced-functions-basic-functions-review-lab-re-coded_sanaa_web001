5a4284168d6cf837573eeb19fd61cb96603fee6f
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-17 20:05:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+}
+ mondayWork=function(activity="go to the office"){
+  return `This Monday, I will ${activity}`;
+}
