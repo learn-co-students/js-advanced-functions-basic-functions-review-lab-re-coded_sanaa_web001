47d7571d0768675c492864895031691c830c8af8
Dhiaa Alshamy <dhodhof@gmail.com>
2020-08-18 02:17:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(par="roller-skate"){
+return "This Saturday, I want to "+par+"!";
+}
+function mondayWork(par="go to the office"){
+return "This Monday, I will "+par+".";
+}
+
+function wrapAdjective(flair="*"){
+  return function(par='special'){
+    return `You are ${flair}${par}${flair}!`
+  }
+}
+let encouragingPromptFunction = wrapAdjective("!!!");
+
+let Calculator={
+  add:function(a,b){
+    return a+b;
+  },
+  subtract:function(a,b){
+    return a-b;
+  },
+  multiply:function(a,b){
+    return a*b;
+  },
+  divide:function(a,b){
+    return a/b;
+  },
+  
+}
+function actionApplyer (arg1,arr){
+  let ret=arg1
+for(let i=0; i<arr.length;i++) {
+ret= arr[i](ret)
+ 
+}
+  return ret;
+}
